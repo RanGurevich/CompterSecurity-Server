7e3de1fea929fd43b5d1c2630b57b22b4b51ed34
Tomer Cohen <tomernado1233@gmail.com>
2025-12-15 10:36:40 +0200

    sql uplouding

--- sqlScripts/security-data_users.sql
+++ sqlScripts/security-data_users.sql
@@ -1,15 +1,58 @@
--- מחיקת הטבלה הישנה כדי להתחיל נקי
-DROP TABLE IF EXISTS users;
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: security-data
+-- ------------------------------------------------------
+-- Server version	8.0.42
 
--- יצירת הטבלה החדשה
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL, -- מקום רחב ל-Bcrypt
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    failed_login_attempts INT DEFAULT 0, -- הכנה לדרישות הפרויקט
-    is_locked BOOLEAN DEFAULT FALSE      -- הכנה לנעילת משתמשים
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-SELECT * FROM users;
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `failed_login_attempts` int DEFAULT '0',
+  `is_locked` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'tomernado','tomernado1233@gmail.com','$2b$10$5Mw47ZnpYdayatQaTtEVlOKU.qmK4jM8Zu3P3dxw0JpHpXlLrmPfe','2025-12-10 13:04:21',0,0),(2,'tomernado1','tomernado123@walla.com','$2b$10$D1UOTOJqPzvY9uaa8xSZ1OEiWTB3p9hFV/J2qFUfkOAXZER6AtHP6','2025-12-10 13:06:14',0,0),(5,'tomernado4','tomernado123@walla.com4','$2b$10$/FmuG0Rziu7y9iUw3fAxoOQhSFQB1S5XghqAUzWni6sPN5yKLgYxe','2025-12-10 17:04:20',0,0),(6,'tomernadott','tomernado123@gmail.com','$2b$10$73puronQawAxL4HWbBAXfehlADezz5p6wh/7FgSTkKlTTXuSSFi3S','2025-12-10 17:05:36',0,0),(9,'tomernadonnn','tomernado123@gmail.comt','$2b$10$0O4Xl357m83gXcJXW6rEV.bUCXiiw3wYRbDcrQu015s5VVacF78QK','2025-12-10 17:07:49',0,0),(10,'tomernadofx','tomernado1233@gmail.comb','$2b$10$LAdITYllH2jOUmcJ/Nyt0u7OFxD0SVU3lY.PAAcLzaeNfL8zAE6ra','2025-12-10 18:06:34',0,0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-15 10:35:50
